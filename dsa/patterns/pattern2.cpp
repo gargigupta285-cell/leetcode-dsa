d8bef452faf982782a8852431877cbd222985b96
gargi <gargigupta285@gmail.com>
2026-06-25 21:52:37 +0530

    pattern

---
+++ dsa/patterns/pattern2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    for (int i = 1; i <= n; i++) {
+
+        // Print stars
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+
+        // Move to next line
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    printPattern(n);
+
+    return 0;
+}
